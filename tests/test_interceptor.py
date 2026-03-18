a08764792a0fa54c7e89dae4bb89cf7131de52c8
LateFR <183198508+LateFR@users.noreply.github.com>
2026-03-18 18:12:40 +0000

    test(interceptor): cover explicit end layer

--- tests/test_interceptor.py
+++ tests/test_interceptor.py
@@ -24,3 +24,20 @@ def test_flush_layer_order():
     for key in ["prefill", "token"]:
         assert torch.allclose(acts[key][0, 0, :], torch.full((hidden_dim,), ids[1]))
         assert torch.allclose(acts[key][0, 1, :], torch.full((hidden_dim,), ids[0]))
+        
+def test_interceptor_explicit_end_layer():
+    target_end_layer = 200
+    
+    class FakeLayer(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+        def register_forward_hook(self, fn): pass
+    
+    class FakeModel:
+        class model:
+            layers = torch.nn.ModuleList([FakeLayer() for _ in range(target_end_layer)])
+    interceptor = Interceptor(FakeModel(), end_layer=target_end_layer)
+    interceptor.attach()
+    
+    assert interceptor.end_layer == target_end_layer
+    
